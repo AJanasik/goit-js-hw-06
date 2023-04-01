8a9f93b933e2aadc9caa428893321ce57281efdc
AJanasik <117896853+AJanasik@users.noreply.github.com>
2023-04-01 21:27:18 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,25 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const divElement = document.querySelector("#boxes");
+const quantity = document.querySelector("input[type='number']");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+const createBox = (amount) => {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = 30 + 10 * i + `px`;
+    box.style.height = 30 + 10 * i + `px`;
+    box.style.backgroundColor = getRandomHexColor();
+    divElement.append(box);
+  }
+};
+
+btnCreate.addEventListener("click", () => createBox(quantity.value));
+
+function destroyBoxes() {
+  quantity.value = null;
+  divElement.innerHTML = null;
+}
+btnDestroy.addEventListener(`click`, destroyBoxes);
